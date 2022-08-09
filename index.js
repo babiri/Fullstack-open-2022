c9e00c234bb16adbfb318858b2ab9185681f3a87
pedro silva <pedrofjrsilva@gmail.com>
2022-08-09 14:38:19 +0100

    finished exercise 3.4

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-
 })
 
 app.get('/info', (request, response) => {
@@ -48,6 +47,40 @@ app.get('/info', (request, response) => {
   response.send(page)
 })
 
-const PORT = 3001
-app.listen(PORT)
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+const generateId = () => getRandomInt(1, 10000000);
+
+app.post('/api/persons', (request, response) => {
+  const query = request.query;
+  if (!query.name || !query.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const randomId = generateId()
+  const person = {
+    name: query.name,
+    number: query.number,
+    id: randomId
+  }
+
+  persons = persons.concat(person);
+
+  response.json(person);
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+})
+
+const PORT = 3001;
+app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
